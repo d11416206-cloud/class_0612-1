79663ec9f64f8abc3192b8a4579b95be2fd7d19f
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-06-12 10:27:03 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+delegate void MyDel(int a, int b);
+
+public class MyMath
+{
+    public static string str = "";
+	public static void Add (int a, int b)
+	{
+		str += "\n" + (a + b);
+	}
+
+    public static void Sub(int a, int b)
+    {
+        str += "\n" + (a - b);
+    }
+
+    public static void Mul(int a, int b)
+    {
+        str += "\n" + (a * b);
+    }
+
+    public static void Div(int a, int b)
+    {
+        str += "\n" + (a / b);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace class_0612_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel hander = new MyDel(MyMath.Mul);
+            hander += new MyDel(MyDel.Sub);
+            hander += new MyDel(MyDel.Add);
+            hander += new MyDel(MyDel.Div);
+            hander -= new MyDel(MyMath.Mul);
+            hander -= new MyDel(MyDel.Div);
+            hander(10, 20);
+            Console.WriteLine(MyMath.str);
+
+        }
+    }
+}
